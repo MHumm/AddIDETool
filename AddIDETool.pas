a16e7941b2f78ca6223c017e6a9a139490087208
Markus Humm <markus.humm@googlemail.com>
2021-08-28 17:37:50 +0200

    First version with Add, Delete and IsInMenu.

---
+++ AddIDETool.pas
@@ -0,0 +1,312 @@
+{*****************************************************************************
+  The AddIDETools team (see file NOTICE.txt) licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License. A copy of this licence is found in the root directory
+  of this project in the file LICENCE.txt or alternatively at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing,
+  software distributed under the License is distributed on an
+  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  KIND, either express or implied.  See the License for the
+  specific language governing permissions and limitations
+  under the License.
+*****************************************************************************}
+unit AddIDETool;
+
+interface
+
+uses
+  System.SysUtils,
+  System.Win.Registry,
+  System.Classes,
+  Winapi.Windows;
+
+type
+  /// <summary>
+  ///   Class for adding something to or deleteing from the IDE's tools menu.
+  /// </summary>
+  TAddIDETool = class(TObject)
+  strict private
+    /// <summary>
+    ///   Provides access to the Windows registry
+    /// </summary>
+    FRegistry : TRegistry;
+
+    /// <summary>
+    ///   Search if a certain tool path is already listed under tools and if yes
+    ///   returns the registry path of that registry entry
+    /// </summary>
+    /// <param name="Path">
+    ///   Path to search for
+    /// </param>
+    /// <param name="RegPath">
+    ///   Path to the registry key of the IDE version under which to search
+    /// </param>
+    /// <returns>
+    ///   Registry path of the registry node for the tools entry which already
+    ///   exists for the path or empty string if the path is not yet added as
+    ///   tools entry
+    /// </returns>
+    function SearchForToolsPath(Path: string; const RegPath: string):string;
+    /// <summary>
+    ///   Adds a tool to the tools menu of selected Rad Studio IDEs
+    /// </summary>
+    /// <param name="Params">
+    ///   Command line params the IDE shall pass to the tool
+    /// </param>
+    /// <param name="Path">
+    ///   Path and file name of the tool
+    /// </param>
+    /// <param name="Title">
+    ///   Name of the tool in the menu
+    /// </param>
+    /// <param name="WorkingDir">
+    ///   Path which the IDE sets as working dir before calling the tool
+    /// </param>
+    /// <param name="RegPath">
+    ///   Registry key name of the tools menu entries for the processed IDE version
+    /// </param>
+    procedure DoAddModifyTool(const Params, Path, Title, WorkingDir, RegPath: string);
+  public
+    /// <summary>
+    ///   Initialize internal fields
+    /// </summary>
+    constructor Create;
+    /// <summary>
+    ///   Free internal fields
+    /// </summary>
+    destructor  Destroy; override;
+
+    /// <summary>
+    ///   Returns a list of all installed IDE versions
+    /// </summary>
+    /// <returns>
+    ///   List of all installed IDE versions. The list is always created even if
+    ///   no IDE is installed.
+    /// </returns>
+    function GetIDEVersionsList: TStringList;
+    /// <summary>
+    ///   Adds a tool to the tools menu of selected Rad Studio IDEs
+    /// </summary>
+    /// <param name="Params">
+    ///   Command line params the IDE shall pass to the tool
+    /// </param>
+    /// <param name="Path">
+    ///   Path and file name of the tool
+    /// </param>
+    /// <param name="Title">
+    ///   Name of the tool in the menu
+    /// </param>
+    /// <param name="WorkingDir">
+    ///   Path which the IDE sets as working dir before calling the tool
+    /// </param>
+    /// <param name="IDEVersions">
+    ///   List of IDE versions the tool shall get added to. If the list contains
+    ///   versions not installed they will be ignored. Best is to call
+    ///   GetIDEVersionsList and pass that one.
+    /// </param>
+    procedure AddTool(const Params, Path, Title, WorkingDir: string;
+                      IDEVersions: TStringList);
+    /// <summary>
+    ///   Remove a tools menu entry for a given tool
+    /// </summary>
+    /// <param name="Path">
+    ///   Path and file name of the tool to remove, it will be referenced by that
+    /// </param>
+    /// <param name="IDEVersions">
+    ///   List of IDE versions the tool shall get deleted from. If the list contains
+    ///   versions not installed they will be ignored. Best is to call
+    ///   GetIDEVersionsList and pass that one.
+    /// </param>
+    procedure DeleteTool(const Path: string; IDEVersions: TStringList);
+
+    /// <summary>
+    ///   Checks whether a certain application is listed in the tools menu of
+    ///   a certain IDE version
+    /// </summary>
+    /// <param name="Path">
+    ///   Path and file name of the tool to look for
+    /// </param>
+    /// <param name="IDEVersion">
+    ///   IDE version to check, as returned by GetIDEVersionList
+    /// </param>
+    /// <returns>
+    ///   true if a tool with that path is listed under Tools menu for the
+    ///   given IDE version
+    /// </returns>
+    function IsInMenu(const Path, IDEVersion: string):Boolean;
+  end;
+
+implementation
+
+const
+  IDERootKey       = 'SOFTWARE\Embarcadero\BDS';
+  ToolsKey         = 'Transfer';
+
+{ TAddIDETool }
+
+procedure TAddIDETool.AddTool(const Params, Path, Title, WorkingDir: string;
+  IDEVersions: TStringList);
+var
+  IDEVersion      : string;
+  ExistingRegPath : string;
+  RegPath         : string;
+begin
+  Assert(Assigned(IDEVersions), 'Not created list of IDE versions has been passed');
+  Assert(Path <> '', 'Empty path/file name has been specified');
+  Assert(Title <> '', 'Empty title has been specified');
+
+  for IDEVersion in IDEVersions do
+  begin
+    // Skip versions older than D2009
+    if (StrToFloat(IDEVersion, TFormatSettings.Create('en-US'))  >= 6.0) then
+    begin
+      // if registry path to the tools menu list exists
+      RegPath := IDERootKey + '\' + IDEVersion + '\' + ToolsKey;
+      if FRegistry.OpenKey(RegPath, false) then
+      begin
+        FRegistry.CloseKey;
+        // Check if that path is already listed
+        ExistingRegPath := SearchForToolsPath(Path, RegPath);
+
+        if (ExistingRegPath = '') then
+        begin
+          RegPath := RegPath + '\' + Title;
+          DoAddModifyTool(Params, Path, Title, WorkingDir, RegPath);
+        end
+        else
+          DoAddModifyTool(Params, Path, Title, WorkingDir, ExistingRegPath);
+      end;
+    end;
+  end;
+end;
+
+constructor TAddIDETool.Create;
+begin
+  inherited;
+
+  FRegistry         := TRegistry.Create;
+  FRegistry.RootKey := HKEY_CURRENT_USER;
+end;
+
+procedure TAddIDETool.DeleteTool(const Path: string; IDEVersions: TStringList);
+var
+  IDEVersion      : string;
+  ExistingRegPath : string;
+  RegPath         : string;
+begin
+  Assert(Assigned(IDEVersions), 'Not created list of IDE versions has been passed');
+  Assert(Path <> '', 'Empty path/file name has been specified');
+
+  for IDEVersion in IDEVersions do
+  begin
+    // Skip versions older than D2009
+    if (StrToFloat(IDEVersion, TFormatSettings.Create('en-US'))  >= 6.0) then
+    begin
+      // if registry path to the tools menu list exists
+      RegPath := IDERootKey + '\' + IDEVersion + '\' + ToolsKey;
+      if FRegistry.OpenKey(RegPath, false) then
+      begin
+        FRegistry.CloseKey;
+        // Check if that path is already listed
+        ExistingRegPath := SearchForToolsPath(Path, RegPath);
+
+        if (ExistingRegPath <> '') then
+          FRegistry.DeleteKey(ExistingRegPath);
+      end;
+    end;
+  end;
+end;
+
+destructor TAddIDETool.Destroy;
+begin
+  FRegistry.Free;
+
+  inherited;
+end;
+
+procedure TAddIDETool.DoAddModifyTool(const Params, Path, Title, WorkingDir,
+  RegPath: string);
+begin
+  if FRegistry.OpenKey(RegPath, true) then
+  begin
+    try
+      FRegistry.WriteString('Params', Params);
+      FRegistry.WriteString('Path', Path);
+      FRegistry.WriteString('Title', Title);
+      FRegistry.WriteString('WorkingDir', WorkingDir);
+    finally
+      FRegistry.CloseKey;
+    end;
+  end;
+end;
+
+function TAddIDETool.GetIDEVersionsList: TStringList;
+begin
+  Result := TStringList.Create;
+
+  if FRegistry.OpenKey(IDERootKey, false) then
+  begin
+    try
+      FRegistry.GetKeyNames(Result);
+      FRegistry.CloseKey;
+    except
+      On e:exception do
+        OutputDebugString(PWideChar('Failure retrieving all installed IDE '+
+                                    'versions: ' + e.Message));
+    end;
+  end;
+end;
+
+function TAddIDETool.IsInMenu(const Path, IDEVersion: string): Boolean;
+begin
+  Result := SearchForToolsPath(Path, IDERootKey+'\'+IDEVersion+'\'+ToolsKey) <> '';
+end;
+
+function TAddIDETool.SearchForToolsPath(Path: string;
+                                        const RegPath: string): string;
+var
+  ToolsKeys : TStringList;
+  Registry  : TRegistry;
+  Tool      : string;
+  ReadPath  : string;
+begin
+  Assert(Path <> '', 'Empty path specified');
+  Assert(RegPath <> '', 'Empty registry path specified');
+
+  Result := '';
+  Path   := UpperCase(Path);
+
+  ToolsKeys := TStringList.Create;
+  Registry  := TRegistry.Create;
+  Registry.RootKey := HKEY_CURRENT_USER;
+  try
+    if Registry.OpenKey(RegPath, false) then
+    begin
+      Registry.GetKeyNames(ToolsKeys);
+      Registry.CloseKey;
+
+      for Tool in ToolsKeys do
+      begin
+        if Registry.OpenKey(RegPath + '\' + Tool, false) then
+        begin
+          ReadPath := UpperCase(Registry.ReadString('Path'));
+          Registry.CloseKey;
+          if (ReadPath = Path) then
+          begin
+            Result := RegPath + '\' + Tool;
+            Break;
+          end;
+        end;
+      end;
+    end;
+  finally
+    ToolsKeys.Free;
+    Registry.Free;
+  end;
+end;
+
+end.
---
+++ AddIDEToolProject.dpr
@@ -0,0 +1,62 @@
+// Small demo program of the usage of the unit
+
+{*****************************************************************************
+  The AddIDETools team (see file NOTICE.txt) licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License. A copy of this licence is found in the root directory
+  of this project in the file LICENCE.txt or alternatively at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing,
+  software distributed under the License is distributed on an
+  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  KIND, either express or implied.  See the License for the
+  specific language governing permissions and limitations
+  under the License.
+*****************************************************************************}
+program AddIDEToolProject;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils,
+  System.Classes,
+  AddIDETool in 'AddIDETool.pas';
+
+var
+  ToolAdder   : TAddIDETool;
+  IDEVersions : TStringList;
+
+begin
+  try
+    ToolAdder := TAddIDETool.Create;
+    try
+      IDEVersions := ToolAdder.GetIDEVersionsList;
+
+      if ToolAdder.IsInMenu('D:\Projekte\DECGitMaster\Compiled\BIN_IDExx.x_Win32__Demos\Hash_FMX.exe',
+                            IDEVersions[0]) then
+        WriteLn('Is in tools')
+      else
+        WriteLn('Is not in tools');
+
+//      ToolAdder.DeleteTool('D:\Projekte\DECGitMaster\Compiled\BIN_IDExx.x_Win32__Demos\Hash_FMX.exe', IDEVersions);
+
+      ToolAdder.AddTool('',
+                        'D:\Projekte\DECGitMaster\Compiled\BIN_IDExx.x_Win32__Demos\Hash_FMX.exe',
+                        'FMXHashDemo',
+                        'D:\Projekte\DECGitMaster\Compiled\BIN_IDExx.x_Win32__Demos',
+                        IDEVersions);
+    finally
+      ToolAdder.Free;
+    end;
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+
+  ReadLn;
+end.
